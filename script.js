304131a1973df61b521d4c89f4326a72bad74fcf
TheBIGBOSS3826 <125705611+TheBIGBOSS3826@users.noreply.github.com>
2026-05-05 11:15:25 -0400

    added + to allow multiple lines in my javascript. ddded js paragrpah

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 let header = document.createElement("h1");
 header.textContent = "George's Spring Flowers";
 document.body.appendChild(header);
+
+let paragraph = document.createElement("p");
+paragraph.textContent = "Georges flowers are pretty cool. Never quite as pretty as the real thing, but they are pretty cool. They are made with javascript code " + 
+    "which is pretty cool.";
+document.body.appendChild(paragraph);
