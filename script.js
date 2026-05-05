cf06a9fae29ad1518d117f8e74e85ba44b24c62b
TheBIGBOSS3826 <125705611+TheBIGBOSS3826@users.noreply.github.com>
2026-05-05 11:18:31 -0400

    added js list

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ let paragraph = document.createElement("p");
 paragraph.textContent = "Georges flowers are pretty cool. Never quite as pretty as the real thing, but they are pretty cool. They are made with javascript code " + 
     "which is pretty cool.";
 document.body.appendChild(paragraph);
+
+let item1 = document.createElement("li");
+item1.textContent = "Giant Pickle Flower, made with javascript code and Georges imagination.";
+
+let item2 = document.createElement("li");
+item2.textContent = "Red flower. Made with javascript code and Georges imagination.";
+
+let item3 = document.createElement("li");
+item3.textContent = "Daffodil. Not made with javascript code, but still pretty cool.";
